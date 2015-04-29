f56401e0cdcfc7bc97a1efa3104cb87fa99f39d7
Michael Do <mdo3739@gmail.com>
2015-04-28 23:02:00 -0500

    Users can now sign up and in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
 	def index
-
+		@new_user = User.new
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
 	devise_for :users
+  get 'welcome/index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
