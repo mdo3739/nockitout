b9c6624d77e8be44a9750d49b12f0ea231490b1f
Michael Do <mdo3739@gmail.com>
2015-04-30 20:15:00 -0500

    Finished making seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = User.new(email: "admin@example.com", password: "helloworld")
+admin.skip_confirmation!
+admin.save!
+member = User.new(email: "member@example.com", password: "helloworld")
+member.skip_confirmation!
+member.save!
+
+3.times do 
+	user = User.new(
+		name: 		Faker::Name.name,
+		email: 		Faker::Internet.safe_email,
+		password: Faker::Internet.password
+	)
+	user.skip_confirmation!
+	user.save!
+end
+
+25.times do
+	Item.create!(
+		user: User.all.sample,
+		name: Faker::Lorem.sentence
+	)
+end
+
+puts "Seed Successful"
+puts "There are #{User.all.count} users"
+puts "and #{Item.all.count} things to do"
